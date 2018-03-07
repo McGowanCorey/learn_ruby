9621231de38fc77c7ba5becde2ce408c603daf45
McGowanCorey <32914191+McGowanCorey@users.noreply.github.com>
2018-03-06 18:37:32 -0600

    Update calculator.rb

--- 02_calculator/calculator.rb
+++ 02_calculator/calculator.rb
@@ -1,1 +1,36 @@
-#write your code here
+def add(r,b)
+  r+b
+end
+
+def subtract(r,b)
+  r-b
+end
+
+def sum(array)
+	sum = 0
+	array.each { |r| sum += r }
+	return sum
+end
+
+def multiply(*arguments)
+	product = 1
+
+	arguments.each do |x|
+		product *= x
+	end
+
+	return product
+end
+
+def power(r,b)
+  r**b
+end
+
+def factorial(r)
+	total = 1
+	while (r > 1)
+		total *= r
+		r -= 1
+	end
+	total
+end
